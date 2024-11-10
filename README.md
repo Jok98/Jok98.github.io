b99e7b43f70d776b34124eb2c58497b693bffadc
Jok98 <matteo.moi1998@gmail.com>
2024-11-10 19:25:35 +0100

    update script.js add create_json.sh to generate directories.json file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Jok98.github.io
+
+Before push to github
+```bash
+./create_json.sh
+```
+to recreate directories.json file
+
+---
+
+To test locally
+```bash
+jekyll build
+```
---
+++ create_json.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Directory di partenza
+BASE_DIR="notes"
+
+# File di output JSON
+OUTPUT_FILE="directories.json"
+
+# Funzione ricorsiva per generare il JSON
+generate_json() {
+  local current_dir="$1"
+  echo "  {"
+  echo "    \"name\": \"$(basename "$current_dir")\","
+
+  # Elenca i file diretti
+  local files=($(find "$current_dir" -mindepth 1 -maxdepth 1 -type f | sort))
+  if [ ${#files[@]} -gt 0 ]; then
+    echo "    \"contents\": ["
+    for file in "${files[@]}"; do
+      local filename=$(basename "$file")
+      local relative_path="${file/$BASE_DIR\//}"
+      echo "      {"
+      echo "        \"content\": \"$filename\","
+      echo "        \"link\": \"$relative_path\""
+      echo "      },"
+    done | sed '$s/,$//'
+    echo "    ],"
+  else
+    echo "    \"contents\": [],"
+  fi
+
+  # Elenca le sottocartelle
+  local folders=($(find "$current_dir" -mindepth 1 -maxdepth 1 -type d | sort))
+  if [ ${#folders[@]} -gt 0 ]; then
+    echo "    \"subsections\": ["
+    for folder in "${folders[@]}"; do
+      generate_json "$folder"
+      echo ","
+    done | sed '$s/,$//'
+    echo "    ]"
+  else
+    echo "    \"subsections\": []"
+  fi
+
+  echo "  }"
+}
+
+# Genera il file JSON
+echo "[" > "$OUTPUT_FILE"
+generate_json "$BASE_DIR" >> "$OUTPUT_FILE"
+echo "]" >> "$OUTPUT_FILE"
+
+echo "Struttura JSON generata in $OUTPUT_FILE"
