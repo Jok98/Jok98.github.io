da678ad08f6f6197458de1d2ad6c5e53f86066ce
Jok98 <matteo.moi1998@gmail.com>
2025-10-06 01:25:51 +0200

    style: add back-to-top button for improved navigation

--- assets/script/script.js
+++ assets/script/script.js
@@ -169,3 +169,27 @@ document.addEventListener("DOMContentLoaded", () => {
     return details;
   }
 });
+
+// Get the button
+let mybutton = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (mybutton) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+if (mybutton) {
+  mybutton.addEventListener("click", function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  });
+}
